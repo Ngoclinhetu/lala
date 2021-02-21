4417ec33b7e56eac1ea0d1c8aaa1c66980c6a882
anh linh <phamthingoclinh134@gmail.com>
2021-02-21 13:29:37 +0100

    fisrt commit

---
+++ TP1exo1.py.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 27 15:27:46 2021
+
+@author: phamt
+"""
+
+
+def dec2bin(n):
+    result = ""
+    
+    while n > 0:
+       r = n % 2
+       n = n // 2
+       result = str(r) + result
+    return result   
+    
+       
+    
+x = int(input("enter a number:"))
+print(dec2bin(x))
+
+    
+    
+    
